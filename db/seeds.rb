8845f99d612f6e7251b9231b5955ca3ff901ba07
Cotter Phinney <cotter.phinney@gmail.com>
2018-03-28 03:05:47 -0400

    more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,15 +16,30 @@ Movie.create(title: 'Day of the Dead',
 			 year: Date.parse('July 19, 1985'),
 			 cover_image: 'day-of-the-dead.jpg')
 
-Movie.create(title: 'Black Christmas', 
-			 filename: 'https://www.youtube.com/watch?v=Ii2L702LA78', 
-			 year: Date.parse('December 20, 1974'),
-			 cover_image: 'black-christmas.jpg')
+Movie.create(title: 'Street Trash', 
+			 filename: 'https://www.youtube.com/watch?v=4sLldf6IdYI', 
+			 year: Date.parse('September 16, 1987'),
+			 cover_image: 'street-trash.jpg')
 
-Movie.create(title: 'Elvira\'s 1986 MTV Halloween Special', 
-			 filename: 'https://www.youtube.com/embed/fYZccLA5HBE', 
-			 year: Date.parse('October 31, 1986'),
-			 cover_image: 'elviras-mtv-halloween-special.jpg')
+Movie.create(title: 'Tenebre', 
+			 filename: 'https://www.youtube.com/watch?v=LSWdacqHKR4', 
+			 year: Date.parse('February 13, 1987'),
+			 cover_image: 'tenebre.jpg')
+
+Movie.create(title: 'Creepers aka Phenomena', 
+			 filename: 'https://www.youtube.com/watch?v=h7pXN2-wB34', 
+			 year: Date.parse('August 2, 1985'),
+			 cover_image: 'creepers.jpg')
+
+Movie.create(title: 'Demons', 
+			 filename: 'https://www.youtube.com/watch?v=kWamJZ47EpA', 
+			 year: Date.parse('May 30, 1986'),
+			 cover_image: 'demons.jpg')
+
+Movie.create(title: 'Demons 2', 
+			 filename: 'https://www.youtube.com/watch?v=nvFUF7ceags', 
+			 year: Date.parse('February 13, 1987'),
+			 cover_image: 'demons-2.jpg')
 
 Movie.create(title: 'City of the Living Dead', 
 			 filename: 'https://www.youtube.com/watch?v=HqRcAv6I1WA', 
@@ -41,15 +56,30 @@ Movie.create(title: 'TerrorVision',
 			 year: Date.parse('February 14, 1986'),
 			 cover_image: 'terror-vision.jpg')
 
+Movie.create(title: 'Class of 1984', 
+			 filename: 'https://www.youtube.com/watch?v=wb0MXivyeuA', 
+			 year: Date.parse('July 1, 1982'),
+			 cover_image: 'class-of-1984.jpg')
+
+Movie.create(title: 'Hellraiser', 
+			 filename: 'https://www.youtube.com/watch?v=0qTOSgV38WM', 
+			 year: Date.parse('September 18, 1987'),
+			 cover_image: 'hellraiser.png')
+
+Movie.create(title: 'Hellbound: Hellraiser II', 
+			 filename: 'https://www.youtube.com/watch?v=pZNUXuji0mU', 
+			 year: Date.parse('December 23, 1988'),
+			 cover_image: 'hellraiser2.jpg')
+
+Movie.create(title: 'Prom Night', 
+			 filename: 'https://www.youtube.com/watch?v=6jPhpTeW0ws', 
+			 year: Date.parse('July 18, 1980'),
+			 cover_image: 'prom-night.jpg')
+
 Movie.create(title: 'City of the Walking Dead', 
 			 filename: 'https://www.youtube.com/watch?v=h7pXN2-wB34', 
 			 year: Date.parse('November 18, 1983'),
 			 cover_image: 'city-of-the-walking-dead.png')
-
-Movie.create(title: 'Creepers aka Phenomena', 
-			 filename: 'https://www.youtube.com/watch?v=h7pXN2-wB34', 
-			 year: Date.parse('August 2, 1985'),
-			 cover_image: 'creepers.jpg')
 
 Movie.create(title: 'Graduation Day', 
 			 filename: 'https://www.youtube.com/watch?v=h7pXN2-wB34', 
@@ -61,15 +91,40 @@ Movie.create(title: 'Nightmare',
 			 year: Date.parse('October 23, 1981'),
 			 cover_image: 'nightmare.jpg')
 
+Movie.create(title: 'Pieces', 
+			 filename: 'https://www.youtube.com/watch?v=xYE6kHdxQCI', 
+			 year: Date.parse('September 23, 1983'),
+			 cover_image: 'pieces.png')
+
 Movie.create(title: 'Zombie Nightmare', 
 			 filename: 'https://www.youtube.com/watch?v=8D4o1GYCsCU', 
 			 year: Date.parse('March 7, 1988'),
 			 cover_image: 'zombie-nightmare.jpg')
 
-Movie.create(title: 'Popcorn', 
-			 filename: 'https://www.youtube.com/watch?v=SKmCVRWJQkg', 
-			 year: Date.parse('February 1, 1991'),
-			 cover_image: 'popcorn.jpg')
+Movie.create(title: 'Trick or Treat', 
+			 filename: 'https://www.youtube.com/watch?v=hofayMaAAZY', 
+			 year: Date.parse('October 24, 1986'),
+			 cover_image: 'trick-or-treat.jpg')
+
+Movie.create(title: 'Elvira\'s 1986 MTV Halloween Special', 
+			 filename: 'https://www.youtube.com/embed/fYZccLA5HBE', 
+			 year: Date.parse('October 31, 1986'),
+			 cover_image: 'elviras-mtv-halloween-special.jpg')
+
+Movie.create(title: 'Black Christmas', 
+			 filename: 'https://www.youtube.com/watch?v=Ii2L702LA78', 
+			 year: Date.parse('December 20, 1974'),
+			 cover_image: 'black-christmas.jpg')
+
+Movie.create(title: 'Christmas Evil', 
+			 filename: 'https://www.youtube.com/watch?v=NTBmdVbAknY', 
+			 year: Date.parse('November 1, 1980'),
+			 cover_image: 'christmas-evil.jpg')
+
+Movie.create(title: 'The Omen', 
+			 filename: 'https://www.youtube.com/watch?v=P87-_Y4H_1c', 
+			 year: Date.parse('June 25, 1976'),
+			 cover_image: 'the-omen.jpg')
 
 Movie.create(title: 'The Slayer', 
 			 filename: 'https://www.youtube.com/watch?v=T-njvUxn7h8', 
@@ -96,6 +151,11 @@ Movie.create(title: 'Night of the Living Dead',
 			 year: Date.parse('October 1, 1968'),
 			 cover_image: 'night-of-the-living-dead.jpg')
 
+Movie.create(title: 'Popcorn', 
+			 filename: 'https://www.youtube.com/watch?v=SKmCVRWJQkg', 
+			 year: Date.parse('February 1, 1991'),
+			 cover_image: 'popcorn.jpg')
+
 Movie.create(title: 'The Driller Killer', 
 			 filename: 'https://www.youtube.com/embed/jsRk3AeUQ3Y', 
 			 year: Date.parse('June 15, 1979'),
@@ -110,11 +170,6 @@ Movie.create(title: 'Oasis of the Zombies',
 			 filename: 'https://www.youtube.com/embed/cvY5age1rJ8', 
 			 year: Date.parse('April 21, 1982'),
 			 cover_image: 'oasis-of-the-zombies.jpg')
-
-Movie.create(title: 'Trick or Treat', 
-			 filename: 'https://www.youtube.com/watch?v=hofayMaAAZY', 
-			 year: Date.parse('October 24, 1986'),
-			 cover_image: 'trick-or-treat.jpg')
 
 Movie.create(title: 'Return of the Alien\'s Deadly Spawn', 
 			 filename: 'https://www.youtube.com/watch?v=nGWJL2GDwVA', 
@@ -136,6 +191,11 @@ Movie.create(title: 'House II: The Second Story',
 			 year: Date.parse('August 28, 1987'),
 			 cover_image: 'house-2.jpg')
 
+Movie.create(title: 'House III: The Horror Show', 
+			 filename: 'https://www.youtube.com/watch?v=GZfb4OKWmPs', 
+			 year: Date.parse('April 28, 1989'),
+			 cover_image: 'house-3.jpg')
+
 Movie.create(title: 'The Burning', 
 			 filename: 'https://www.youtube.com/watch?v=wcS28fFS4pE', 
 			 year: Date.parse('May 8, 1981'),
@@ -146,22 +206,52 @@ Movie.create(title: 'Night of the Demons',
 			 year: Date.parse('September 9, 1988'),
 			 cover_image: 'night-of-the-demons.jpg')
 
-Movie.create(title: 'Class of 1984', 
-			 filename: 'https://www.youtube.com/watch?v=wb0MXivyeuA', 
-			 year: Date.parse('July 1, 1982'),
-			 cover_image: 'class-of-1984.jpg')
-
-Movie.create(title: 'Tenebre', 
-			 filename: 'https://www.youtube.com/watch?v=LSWdacqHKR4', 
-			 year: Date.parse('February 13, 1987'),
-			 cover_image: 'tenebre.jpg')
-
-Movie.create(title: 'Prom Night', 
-			 filename: 'https://www.youtube.com/watch?v=6jPhpTeW0ws', 
-			 year: Date.parse('July 18, 1980'),
-			 cover_image: 'prom-night.jpg')
-
 Movie.create(title: 'Elvira: Mistress of the Dark', 
 			 filename: 'https://www.youtube.com/watch?v=i5kOzPEf8-o', 
 			 year: Date.parse('September 30, 1988'),
 			 cover_image: 'elvira-mistress-of-the-dark.jpg')
+
+Movie.create(title: 'Without Warning', 
+			 filename: 'https://www.youtube.com/watch?v=36tUcv6al9s', 
+			 year: Date.parse('September 26, 1980'),
+			 cover_image: 'without-warning.jpg')
+
+Movie.create(title: 'Children of the Corn', 
+			 filename: 'https://www.youtube.com/watch?v=I2ul_vgEl6U', 
+			 year: Date.parse('March 9, 1984'),
+			 cover_image: 'children-of-the-corn.jpg')
+
+Movie.create(title: 'Hell Night', 
+			 filename: 'https://www.youtube.com/watch?v=mV-sMDa72wM', 
+			 year: Date.parse('August 7, 1981'),
+			 cover_image: 'hell-night.jpg')
+
+Movie.create(title: 'The Midnight Hour', 
+			 filename: 'https://www.youtube.com/watch?v=bMMCG--ex_8', 
+			 year: Date.parse('November 1, 1985'),
+			 cover_image: 'the-midnight-hour.jpg')
+
+Movie.create(title: 'Xtro', 
+			 filename: 'https://www.youtube.com/watch?v=bE2mlIpyobE', 
+			 year: Date.parse('December 6, 1982'),
+			 cover_image: 'xtro.jpg')
+
+Movie.create(title: 'Night of the Comet', 
+			 filename: 'https://www.youtube.com/watch?v=IRrniG0RWsA', 
+			 year: Date.parse('November 16, 1984'),
+			 cover_image: 'night-of-the-comet.jpg')
+
+Movie.create(title: 'Spookies', 
+			 filename: 'https://www.youtube.com/watch?v=rKWjjhYEdRM', 
+			 year: Date.parse('May 14, 1986'),
+			 cover_image: 'spookies.jpg')
+
+Movie.create(title: 'Reform School Girls', 
+			 filename: 'https://www.youtube.com/watch?v=-8lW3jWJ4p8', 
+			 year: Date.parse('August 22, 1986'),
+			 cover_image: 'reform-school-girls.jpg')
+
+Movie.create(title: 'Rock \'n\' Roll High School', 
+			 filename: 'https://www.youtube.com/watch?v=WfgQdCeHa0o', 
+			 year: Date.parse('August 4, 1979'),
+			 cover_image: 'rock-n-roll-high-school.jpg')
